5bedc264bb43dc5c80da27a1ee1d0f33740f5120
marijn <marijn@serraict.com>
2026-01-29 14:58:47 +0100

    Add Gherkin formatter

---
+++ lua/plugins/cucumber-format.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      opts.formatters_by_ft.cucumber = { "prettier" }
+      return opts
+    end,
+  },
+}
